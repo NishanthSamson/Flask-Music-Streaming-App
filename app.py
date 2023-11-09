b44f3d6e9ff6759536d9e9d211e056a9d6052d73
NishanthSamson <nishanthsamson@gmail.com>
2023-11-09 23:12:06 +0530

    Configured routes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
@@ -8,19 +7,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/song')
-def song():
-    return render_template('song.html')
+@app.route('/track/<int:id>')
+def track(id):
+    return render_template('track.html', id=id)
 
 
-@app.route('/artist')
-def artist():
-    return render_template('artist.html')
+@app.route('/artist/<int:id>')
+def artist(id):
+    return render_template('artist.html', id=id)
 
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
+@app.route('/user/<int:id>')
+def user(id):
+    return render_template('user.html', id=id)
+
+
+@app.route('/album/<int:id>')
+def album(id):
+    return render_template('album.html', id=id)
 
 
 if (__name__ == '__main__'):
